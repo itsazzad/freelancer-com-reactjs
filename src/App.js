31146858251c8e618c7042e15d1411fd759dd06e
Sazzad Hossain Khan <itsazzad@gmail.com>
2017-11-19 02:23:36 +0600

    Listing projects and contests

--- src/App.js
+++ src/App.js
@@ -5,101 +5,130 @@ import {
     Link
 } from 'react-router-dom';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 import './App.css';
+
+const LIMIT = 1;
 
 class App extends Component {
     render() {
         return (
             <Router>
-                <div>
-                    <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                        <li><Link to="/contests">Contests</Link></li>
-                    </ul>
-
-                    <hr/>
-
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/projects" component={Projects}/>
-                    <Route path="/contests" component={Contests}/>
-                </div>
+                <header>
+                    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+                        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <Link to="/" className="nav-link">Home</Link>
+                                </li>
+                            </ul>
+                            <Route exact path="/" component={Projects}/>
+                        </div>
+                    </nav>
+                </header>
             </Router>
         );
     }
 }
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Home</h2>
-            </div>
-        );
-    }
-}
-
-const work = {
-    projects: function(){
-        return this.data('https://www.freelancer.com/api/projects/0.1/projects/active');
-    },
-    contests: function(){
-        return this.data('https://www.freelancer.com/api/contests/0.1/contests/?statuses[]=active');
-    },
-    data: function(url){
-        return axios.get(url)
-            .then(function (response) {
-                console.log(response);
-                return response;
-            })
-            .catch(function (error) {
-                console.error(error);
-                return false;
-            });
-    },
-};
-
 class Projects extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projects: []
+            projects: [],
+            offset: 0
         };
     }
 
-    componentDidMount() {
-        this.setState(
-            {projects: work.projects()}
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>Projects</h2>
-            </div>
-        );
-    }
-}
-
-class Contests extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            contests: []
-        };
+    loadProjectsFromServer() {
+        const url = `https://www.freelancer.com/api/projects/0.1/projects/active?include_contests=true&compact=true&limit=${LIMIT}&offset=${this.state.offset}`;
+        axios.get(url)
+            .then((response) => {
+                const data = response.data;
+                if (data.status === 'success') {
+                    const projects = data.result.projects;
+                    const pageCount = Math.ceil(data.result.total_count / LIMIT);
+                    this.setState({
+                        projects,
+                        pageCount
+                    });
+                } else {
+                    console.error(data.status);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     componentDidMount() {
-        this.setState(
-            {projects: work.contests()}
-        );
+        this.loadProjectsFromServer();
     }
 
+    handlePageClick = (data) => {
+        let selected = data.selected;
+        let offset = Math.ceil(selected * LIMIT);
+
+        this.setState({offset: offset}, () => {
+            this.loadProjectsFromServer();
+        });
+    };
+
     render() {
+        let jobList = [];
+        for (let i = 0; i < LIMIT; i++) {
+            const job = this.state.projects[i];
+            if (job) {
+                jobList.push(job);
+            }
+        }
+        let jobNodes = jobList.map(function (jobNode, index) {
+            return (
+                <tr key={jobNode.id}>
+                    <td><a href={`https://www.freelancer.com/projects/`}>{jobNode.title}</a></td>
+                    <td>{jobNode.bid_stats.bid_count}/{jobNode.bid_stats.bid_avg}</td>
+                    <td>{jobNode.time_submitted}</td>
+                    <td>{jobNode.currency.code} {jobNode.budget.minimum} - {jobNode.budget.maximum}</td>
+                </tr>
+            );
+        });
         return (
-            <div>
-                <h2>Contests</h2>
+            <div className="container-fluid">
+                <div className="row">
+                    <main role="main" className="col-sm-12 ml-sm-auto col-md-12 pt-3">
+                        <h1>Dashboard</h1>
+                        <div className="table-responsive">
+
+                            <table className="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th>PROJECT/CONTEST</th>
+                                    <th>BIDS/ENTRIES</th>
+                                    <th>STARTED</th>
+                                    <th>PRICE</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {jobNodes}
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+                <div className="row">
+                    <ReactPaginate previousLabel={"previous"}
+                                   nextLabel={"next"}
+                                   breakLabel={<a href="">...</a>}
+                                   breakClassName={"break-me"}
+                                   pageCount={this.state.pageCount}
+                                   marginPagesDisplayed={2}
+                                   pageRangeDisplayed={5}
+                                   onPageChange={this.handlePageClick}
+                                   containerClassName={"pagination"}
+                                   subContainerClassName={"pages pagination"}
+                                   activeClassName={"active"}/>
+
+                </div>
             </div>
         );
     }
